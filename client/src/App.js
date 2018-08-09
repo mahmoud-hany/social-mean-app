167c9669817743097ea289e046ab5dc992f72fac
mahmoud-hany <mahmoud.hany.5220@gmail.com>
2018-08-09 14:06:20 +0200

    connect ReactApp with Node api and setup bootstrap and fontawesom

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>React App by Me</h1>
+        <button className="btn btn-danger">click on me <i className="fas fa-check" ></i></button>
+      </div>
+    );
+  }
+}
+
+export default App;
